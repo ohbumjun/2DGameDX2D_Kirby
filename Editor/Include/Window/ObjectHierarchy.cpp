efa7383edc84644273431a73334f5abc39312e9f
ohbumjun <beomjun0638@hanmail.net>
2021-12-21 12:29:48 +0900

    [Feat] Create ObjectHierarchy Class & Basic LayOuts

---
+++ Editor/Include/Window/ObjectHierarchy.cpp
@@ -0,0 +1,54 @@
+#include "ObjectHierarchy.h"
+#include "IMGUISameLine.h"
+#include "IMGUIListBox.h"
+#include "IMGUILabel.h"
+
+CObjectHierarchy::CObjectHierarchy()
+{
+}
+
+CObjectHierarchy::~CObjectHierarchy()
+{
+}
+
+
+
+bool CObjectHierarchy::Init()
+{
+	CIMGUIWindow::Init();
+
+	CIMGUILabel* Label = AddWidget<CIMGUILabel>("ObjectList", 300.f, 30.f);
+	Label->SetColor(0, 0, 255);
+
+	CIMGUISameLine* Line = AddWidget<CIMGUISameLine>("Line");
+
+	Label = AddWidget<CIMGUILabel>("ComponentList", 300.f, 30.f);
+	Label->SetColor(0, 0, 255);
+
+	m_ObjectListBox = AddWidget<CIMGUIListBox>("ObjectList", 300.f, 500.f);
+	m_ObjectListBox->SetHideName(true);
+	m_ObjectListBox->SetPageItemCount(20);
+	m_ObjectListBox->SetSelectCallback(this, &CObjectHierarchy::SelectObject);
+
+	Line = AddWidget<CIMGUISameLine>("Line");
+
+	m_ComponentListBox = AddWidget<CIMGUIListBox>("ComponentList", 300.f, 500.f);
+	m_ComponentListBox->SetHideName(true);
+	m_ComponentListBox->SetPageItemCount(20);
+	m_ComponentListBox->SetSelectCallback(this, &CObjectHierarchy::SelectComponent);
+
+	return true;
+}
+
+void CObjectHierarchy::Update(float DeltaTime)
+{
+	CIMGUIWindow::Update(DeltaTime);
+}
+
+void CObjectHierarchy::SelectObject(int Index, const char* ObjectName)
+{
+}
+
+void CObjectHierarchy::SelectComponent(int Index, const char* ComponentName)
+{
+}
--- Editor/Include/Window/ObjectHierarchy.h
+++ Editor/Include/Window/ObjectHierarchy.h
@@ -1,7 +1,30 @@
 #pragma once
 #include "IMGUIWindow.h"
+
 class CObjectHierarchy :
     public CIMGUIWindow
 {
+public :
+    CObjectHierarchy();
+    ~CObjectHierarchy();
+private :
+    class CIMGUIListBox* m_ObjectListBox;
+    class CIMGUIListBox* m_ComponentListBox;
+public :
+    CIMGUIListBox* GetObjectListBox() const 
+	{
+		return m_ObjectListBox;
+	}
+    CIMGUIListBox* GetComponentListBox() const
+    {
+        return m_ComponentListBox;
+    }
+public :
+    void SelectObject(int Index, const char* ObjectName);
+    void SelectComponent(int Index, const char* ComponentName);
+public :
+    virtual bool Init() override;
+    virtual void Update(float DeltaTime) override;
 };
+
 
