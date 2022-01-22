35b442b815d70e1c7c22e39af79f55f4640a2239
ohbumjun <beomjun0638@hanmail.net>
2022-01-23 01:38:48 +0900

    [Feat] ( Client ) Create CThreadQueue Class

---
+++ Client2D/Include/Scene/LoadingThread.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Thread.h"
+
+class CLoadingThread :
+    public CThread
+{
+public :
+    CLoadingThread();
+    virtual ~CLoadingThread() override;
+private :
+    
+
+
+};
+
---
+++ GameEngine/Include/ThreadQueue.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "GameInfo.h"
+#include "Sync.h"
+#include <assert.h>
+
+template<typename T, int SIZE = 200>
+class CThreadQueue {
+public :
+	CThreadQueue()
+{
+		m_Head = 0;
+		m_Tail   = 0;
+		m_Size  = 0;
+		m_Capacity = SIZE + 1;
+		InitializeCriticalSection(&m_Crt);
+}
+	~CThreadQueue()
+{
+		DeleteCriticalSection(&m_Crt);
+}
+private :
+	int m_Head;
+	int m_Tail;
+	int m_Size;
+	int m_Capacity;
+	T m_Queue[SIZE + 1];
+	CRITICAL_SECTION m_Crt;
+public :
+	T& front()
+{
+		if (empty())
+			assert(false);
+
+		int Head = (m_Head + 1) % m_Capacity;
+
+		return m_Queue[Head];
+}
+	void pop()
+{
+		if (empty())
+			assert(false);
+
+		m_Head = (m_Head + 1) % m_Capacity;
+
+		--m_Size;
+}
+	void push(T Elem)
+{
+		int Tail = (m_Tail + 1) % m_Capacity;
+
+		if (Tail == m_Head)
+			return;
+
+		m_Queue[Tail] = Elem;
+
+		m_Tail = Tail;
+
+		++m_Size;
+}
+	bool empty()
+{
+		return m_Size == 0;
+}
+	int size()
+{
+		return m_Size;
+}
+	void clear()
+{
+		m_Size  = 0;
+		m_Head = 0;
+		m_Tail    = 0;
+}
+};
