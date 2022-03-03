88cca6d7f88c96f3be7662cf120c6c69ae4e9f13
ohbumjun <beomjun0638@hanmail.net>
2022-03-03 13:18:47 +0900

    [Feat] ( Client ) Apply SideWall Check By CheckSideCollision( ) Function In LifeObject Class

--- GameEngine/Include/GameObject/LifeObject.cpp
+++ GameEngine/Include/GameObject/LifeObject.cpp
@@ -18,7 +18,7 @@ CLifeObject::CLifeObject():
 	m_FallVelocityMax(1000.f),
 	m_GravityAccel(10.f),
 	m_FloorCheck(false),
-	m_SideWallCheck(false),	// Jump
+	m_SideWallCheck(true),	
 	m_JumpAccel(1.1f),
 	m_JumpAccelAccTime(0.f),
 	m_PrevPos{}
@@ -179,7 +179,180 @@ void CLifeObject::CheckCeilingCollision()
 {}
 
 void CLifeObject::CheckSideCollision()
-{}
+{
+	Vector3 m_Pos = GetWorldPos();
+
+	float DirX = m_Pos.x - m_PrevPos.x;
+
+	bool SideCollision = false;
+
+	if (DirX != 0 && m_SideWallCheck)
+	{
+		// 이전 위치, 현재 위치 까지의 이동 범위 안의 모든 Tile 들을 고려할 것이다
+
+		CTileEmptyComponent* TileMap = m_Scene->GetTileEmptyComponent();
+
+		Vector3 WorldScale = GetWorldScale();
+		Vector3 Pivot = GetPivot();
+		Vector3 TileSize = TileMap->GetTileEmptySize();
+
+		Vector3 CurLB = m_Pos - Pivot * WorldScale;
+		Vector3 CurRT = CurLB + WorldScale;
+
+		Vector3 PrevLB = m_PrevPos - Pivot * WorldScale;
+		Vector3 PrevRT = PrevLB + WorldScale;
+
+		int LBIndexX = -1, LBIndexY = -1, RTIndexX = -1, RTIndexY = -1;
+
+		Vector3 ResultLB, ResultRT;
+
+		// 오른쪽으로 이동할 때 --> 왼쪽타일에서 오른쪽 타일 순서대로 조사하기
+		if (DirX >  0.f)
+		{
+			int Index = -1;
+			Vector3 TilePos = {};
+
+			ResultLB.x = PrevLB.x;
+			ResultLB.y = CurLB.y < PrevLB.y ? CurLB.y : PrevLB.y;
+
+			ResultRT.x = CurRT.x;
+			ResultRT.y = CurRT.y > PrevRT.y ? CurRT.y : PrevRT.y;
+
+			LBIndexX = TileMap->GetTileEmptyIndexX(Vector3(ResultLB.x, ResultLB.y, m_Pos.z));
+			LBIndexY = TileMap->GetTileEmptyIndexY(Vector3(ResultLB.x, ResultLB.y, m_Pos.z));
+
+			RTIndexX = TileMap->GetTileEmptyIndexX(Vector3(ResultRT.x, ResultRT.y, m_Pos.z));
+			RTIndexY = TileMap->GetTileEmptyIndexY(Vector3(ResultRT.x, ResultRT.y, m_Pos.z));
+
+			// 범위 고려하기
+			LBIndexX = LBIndexX < 0 ? 0 : LBIndexX;
+			LBIndexY = LBIndexY < 0 ? 0 : LBIndexY;
+
+			RTIndexX = RTIndexX >= TileMap->GetTileCountX() ? TileMap->GetTileCountX() - 1 : RTIndexX;
+			RTIndexY = RTIndexY >= TileMap->GetTileCountY() ? TileMap->GetTileCountY() - 1 : RTIndexY;
+
+			for (int row = LBIndexY; row <= RTIndexY; row++)
+			{
+				for (int col = LBIndexX; col <= RTIndexX; col++)
+				{
+					Index = row * TileMap->GetTileCountX() + col;
+
+					// 예외 처리
+					/*
+					if (TileMap->GetTile(Index)->GetTileOption() !=
+						ETileOption::Wall ||
+						!TileMap->GetTile(Index)->GetSideCollision())
+						continue;
+					 */
+					if (TileMap->GetTileEmpty(Index)->GetTileType() != Tile_Type::Wall)
+						continue;
+
+					TilePos = TileMap->GetTileEmpty(Index)->GetWorldPos();
+
+					// Object 가 Tile 위에 거의 붙어있을 경우에는,
+					// 충돌 처리를 하지 않을 것이다
+					if (TilePos.y - 0.001f <= CurLB.y && CurLB.y <= TilePos.y + 0.001f)
+						continue;
+
+					// 현재 코드 상에서는
+					// Side Collision 먼저 , Bottom Collision
+					// 만약 너무 깊이 파고든다면, Side Collision 무시하고, 중력 적용에 따른
+					// 위로 밀어내기 만을 적용할 예정이다
+					if (CurRT.x - TilePos.x > 8.f)
+						continue;
+
+					// 렉트 충돌
+					if (CurLB.x <= TilePos.x + TileSize.x  &&
+						CurRT.x >= TilePos.x &&
+						CurLB.y <= TilePos.y + TileSize.y &&
+						CurRT.y >= TilePos.y 
+						)
+					{
+						SideCollision = true;
+
+						// 현재 오른쪽으로 이동 중 --> 왼쪽으로 밀어낼 것이다
+						float MoveX = TilePos.x - CurRT.x - 0.01f;
+
+						m_Pos.x += MoveX;
+
+						SetWorldPos(m_Pos);
+
+						// break 문이 필요한가 ?
+						break;
+					}
+				}
+				if (SideCollision)
+					break;
+			}
+		}
+		// 만약 왼쪽으로 이동 중이라면 
+		else
+		{
+			int Index = -1;
+			Vector3 TilePos = {};
+
+			ResultLB.x = CurLB.x;
+			ResultLB.y = CurLB.y < PrevLB.y ? CurLB.y : PrevLB.y;
+
+			ResultRT.x = PrevRT.x;
+			ResultRT.y = CurRT.y > PrevRT.y ? CurRT.y : PrevRT.y;
+
+			LBIndexX = TileMap->GetTileEmptyIndexX(Vector3(ResultLB.x, ResultLB.y, m_Pos.z));
+			LBIndexY = TileMap->GetTileEmptyIndexY(Vector3(ResultLB.x, ResultLB.y, m_Pos.z));
+
+			RTIndexX = TileMap->GetTileEmptyIndexX(Vector3(ResultRT.x, ResultRT.y, m_Pos.z));
+			RTIndexY = TileMap->GetTileEmptyIndexY(Vector3(ResultRT.x, ResultRT.y, m_Pos.z));
+
+			// 범위 고려하기
+			LBIndexX = LBIndexX < 0 ? 0 : LBIndexX;
+			LBIndexY = LBIndexY < 0 ? 0 : LBIndexY;
+
+			RTIndexX = RTIndexX >= TileMap->GetTileCountX() ? TileMap->GetTileCountX() - 1 : RTIndexX;
+			RTIndexY = RTIndexY >= TileMap->GetTileCountY() ? TileMap->GetTileCountY() - 1 : RTIndexY;
+
+			for (int row = LBIndexY; row <= RTIndexY; row++)
+			{
+				for (int col = LBIndexX; col <= RTIndexX; col++)
+				{
+					Index = row * TileMap->GetTileCountX() + col;
+
+					// Wall 이 아니라면 Pass
+					if (TileMap->GetTileEmpty(Index)->GetTileType() != Tile_Type::Wall)
+						continue;
+
+					TilePos = TileMap->GetTileEmpty(Index)->GetWorldPos();
+
+					// 마찬가지로, 바닥에 붙어있다면 체크하지 않는다.
+					if (TilePos.y - 0.001f <= CurLB.y && CurLB.y <= TilePos.y + 0.001f)
+						continue;
+
+					// 너무 깊이 파고들면 Side Collision 은 무시하고, 중력 적용에 따른
+					// 위로 밀어내기 만을 적용할 예정이다.
+					if (TilePos.x + TileSize.x - CurLB.x > 8.f)
+						continue;
+
+					if (CurLB.x <= TilePos.x + TileSize.x && 
+						CurRT.x >= TilePos.x &&
+						CurLB.y <= TilePos.y + TileSize.y &&
+						CurRT.y >= TilePos.y)
+					{
+						SideCollision = true;
+
+						float MoveX = TilePos.x + TileSize.x - CurLB.x + 0.001f;
+
+						m_Pos.x += MoveX;
+
+						SetWorldPos(m_Pos);
+
+						break;
+					}
+				}
+				if (SideCollision)
+					break;
+			}
+		}
+	}
+}
 
 void CLifeObject::Start()
 {
@@ -199,6 +372,9 @@ bool CLifeObject::Init()
 void CLifeObject::Update(float DeltaTime)
 {
 	CGameObject::Update(DeltaTime);
+
+	// 1) 중력 적용 이전에 Side Collision 먼저 적용해야 한다.
+	CheckSideCollision();
 
 	UpdateWhileOffGround(DeltaTime);
 
