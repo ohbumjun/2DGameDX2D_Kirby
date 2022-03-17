46571dcd9a62a222f1b010de1eefc64a05339066
ohbumjun <beomjun0638@hanmail.net>
2022-03-17 12:57:41 +0900

    [Feat] (Client) Create BeamKirbyNormalAttack Class Which Is An Effect Beam Kirby Makes When Normal Attack

--- Client2D/Include/Object/BeamKirbyNormalAttack.h
+++ Client2D/Include/Object/BeamKirbyNormalAttack.h
@@ -1,7 +1,27 @@
 #pragma once
-#include "C:\Users\dhsys\Desktop\DX_Project\Engine\Include\GameObject\GameObject.h"
-class CBeamKirbyAttack :
+#include "GameObject\GameObject.h"
+
+class CBeamKirbyNormalAttack :
     public CGameObject
 {
+    friend class CFireMonster;
+public:
+    CBeamKirbyNormalAttack();
+    CBeamKirbyNormalAttack(const CBeamKirbyNormalAttack& Attack);
+    virtual ~CBeamKirbyNormalAttack() override;
+private:
+    CSharedPtr<class CSpriteComponent> m_Sprite;
+    float m_AttackDir;
+    float m_AttackDistLimit;
+    float m_AttackDistLimitMax;
+protected:
+    void SetRightAttackDir(float YDir);
+    void SetLeftAttackDir(float YDir);
+public:
+    virtual void Start() override;
+    virtual bool Init() override;
+    virtual void Update(float DeltaTime) override;
+private:
+    void CollisionCallback(const CollisionResult& Result);
 };
 
