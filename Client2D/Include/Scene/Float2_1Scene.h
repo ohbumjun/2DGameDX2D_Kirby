b5fbefb98fbc6d34316b107e3f0da48982828293
ohbumjun <beomjun0638@hanmail.net>
2022-03-23 18:14:00 +0900

    [Feat] (Client) Create Float2_1, Float2_2Scene Class Which Each Reprsent Float2_1, Float2_2 Scene

---
+++ Client2D/Include/Scene/Float2_1Scene.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Scene\SceneMode.h"
+
+class CFloat2_1Scene :
+    public CSceneMode
+{
+public:
+	CFloat2_1Scene();
+	virtual ~CFloat2_1Scene() override;
+private:
+	class CUIWindow* m_MainWidget;
+	std::function<void(bool, float)> m_LoadingFunction;
+public:
+	virtual void Start() override;
+	virtual bool Init() override;
+
+private:
+	void CreateMaterial();
+	void CreateAnimationSequence();
+	void CreateSound();
+	void CreateParticle();
+public:
+	virtual void PrepareResources() override;
+public:
+	template<typename T>
+	void SetLoadingFunction(T* Obj, void (T::* Func)(bool, float))
+	{
+		m_LoadingFunction = std::bind(Func, Obj, std::placeholders::_1, std::placeholders::_2);
+	}
+};
+
---
+++ Client2D/Include/Scene/Float2_2Scene.cpp
@@ -0,0 +1,119 @@
+#include "Float2_2Scene.h"
+#include <Scene/CameraManager.h>
+#include "../Object/Player2D.h"
+#include "Scene/Scene.h"
+#include "Scene/SceneResource.h"
+#include "Scene/SceneManager.h"
+#include "../Object/Monster.h"
+#include "../Object/TileMap.h"
+#include "../Object/TileMapEmpty.h"
+#include "../Object/PixelTest.h"
+#include "Scene/ViewPort.h"
+#include "../UI/MainWidget.h"
+#include "../Object/LineContainer.h"
+#include "../Object/EffectSceneChangeAlpha.h"
+
+CFloat2_2Scene::CFloat2_2Scene()
+{}
+
+CFloat2_2Scene::~CFloat2_2Scene()
+{}
+
+void CFloat2_2Scene::Start()
+{
+	CSceneMode::Start();
+
+	// Scene Change È¿°ú
+	CEffectSceneChangeAlpha* Alpha = m_Scene->CreateGameObject<CEffectSceneChangeAlpha>("Alpha");
+
+	Alpha->SetSceneStart(true);
+
+	CGameObject* TileMapEmtpyObject = m_Scene->FindGameObjectByTypeID(typeid(CTileMapEmpty).hash_code());
+
+	SetTileMapEmptyObject(TileMapEmtpyObject);
+
+	m_Scene->SetWorldResolution(TileMapEmtpyObject->GetWorldScale().x, TileMapEmtpyObject->GetWorldScale().y);
+
+	CGameObject* Player2D = m_Scene->FindGameObjectByTypeID(typeid(CPlayer2D).hash_code());
+
+	SetPlayerObject(Player2D);
+
+	m_Scene->GetCameraManager()->GetCurrentCamera()->SetWorldPos(Player2D->GetWorldPos());
+
+	CGameObject* LineContainer = m_Scene->FindGameObjectByTypeID(typeid(CLineContainer).hash_code());
+
+	SetLineContainerObject(LineContainer);
+
+	if (CSceneManager::GetStaticPlayerInfo())
+	{
+		if (m_Scene->GetSceneChangeObject())
+		{
+			Vector3 PlayerSpawnBasePos = m_Scene->GetSceneChangeObject()->GetWorldPos();
+
+			Player2D->SetWorldPos(PlayerSpawnBasePos.x, PlayerSpawnBasePos.y + 100.f, 0.f);
+		}
+	}
+}
+
+bool CFloat2_2Scene::Init()
+{
+	if (!CSceneMode::Init())
+		return false;
+
+	return true;
+}
+
+void CFloat2_2Scene::CreateMaterial()
+{
+	// Particle 
+	m_Scene->GetResource()->CreateMaterial<CMaterial>("BubbleParticleMaterial");
+	CMaterial* BubbleParticleMaterial = m_Scene->GetResource()->FindMaterial("BubbleParticleMaterial");
+	BubbleParticleMaterial->AddTexture(0, (int)Buffer_Shader_Type::Pixel, "BubbleParticleTexture", TEXT("Particle/Bubbles99px.png"));
+	BubbleParticleMaterial->SetShader("ParticleRenderShader");
+	BubbleParticleMaterial->SetRenderState("AlphaBlend");
+
+
+	m_Scene->GetResource()->CreateMaterial<CMaterial>("SpecialChangeMaterial");
+	CMaterial* SpecialChangeParticleMaterial = m_Scene->GetResource()->FindMaterial("SpecialChangeMaterial");
+	SpecialChangeParticleMaterial->AddTexture(0, (int)Buffer_Shader_Type::Pixel, "SpecialChangeParticleTexture", TEXT("Particle/Sparks.png"));
+	SpecialChangeParticleMaterial->SetShader("ParticleRenderShader");
+	SpecialChangeParticleMaterial->SetRenderState("AlphaBlend");
+
+
+	// TileMap
+	m_Scene->GetResource()->CreateMaterial<CMaterial>("DiabloTileMap");
+	CMaterial* TileMapMaterial = m_Scene->GetResource()->FindMaterial("DiabloTileMap");
+	TileMapMaterial->AddTexture(0, (int)Buffer_Shader_Type::Pixel, "DiabloTile", TEXT("Diablos_Lair_Floor_TRS/Diablos_Lair_Floor.png"));
+
+	TileMapMaterial->SetShader("TileMapShader");
+	TileMapMaterial->SetRenderState("AlphaBlend");
+
+	// Back Material
+	m_Scene->GetResource()->CreateMaterial<CMaterial>("BackMaterial");
+	CMaterial* BackMaterial = m_Scene->GetResource()->FindMaterial("BackMaterial");
+	BackMaterial->AddTexture(0, (int)Buffer_Shader_Type::Pixel, "SampleBack", TEXT("LoadingBack.jpg"));
+
+	BackMaterial->SetShader("Mesh2DShader");
+}
+
+void CFloat2_2Scene::CreateAnimationSequence()
+{}
+
+void CFloat2_2Scene::CreateSound()
+{}
+
+void CFloat2_2Scene::CreateParticle()
+{}
+
+void CFloat2_2Scene::PrepareResources()
+{
+	CSceneMode::PrepareResources();
+
+	CreateMaterial();
+
+	CreateAnimationSequence();
+
+	CreateParticle();
+
+	CreateSound();
+}
