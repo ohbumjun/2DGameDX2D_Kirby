550116a5a6de14e7ef7fb3b48d483d2c889fb8b1
ohbumjun <beomjun0638@hanmail.net>
2022-03-28 15:12:02 +0900

    [Feat] (Client) Create SwordBoomerang Class

---
+++ Client2D/Include/Object/SwordBoomerang.cpp
@@ -0,0 +1,165 @@
+#include "SwordBoomerang.h"
+#include "Player2D.h"
+#include "GameObject/LifeObject.h"
+#include "Component/SpriteComponent.h"
+#include "Scene/Scene.h"
+#include "Component/ColliderCircle.h"
+#include "UI/UIDamageFont.h"
+#include "Scene/SceneResource.h"
+#include "Scene/CameraManager.h"
+#include "Animation/AnimationSequence2DInstance.h"
+#include "Animation/AnimationSequence2DData.h"
+#include "FireMonster.h"
+#include "Block.h"
+#include "FireAttackBackEffect.h"
+
+CSwordBoomerang::CSwordBoomerang() :
+	m_ThrowDirDistMax(700.f),
+	m_Return(false),
+	m_SpeedAccel(1.1f),
+	m_InitSpeed(500.f),
+	m_AttackDir(1.f)
+{
+}
+
+CSwordBoomerang::~CSwordBoomerang()
+{}
+
+void CSwordBoomerang::SetAttackDamage(float Damage)
+{
+	m_AttackDamage = Damage;
+}
+
+void CSwordBoomerang::SetAttackDir(float DirX)
+{
+	m_AttackDir = DirX;
+}
+
+void CSwordBoomerang::UpdateBoomerangMovement(float DeltaTime)
+{}
+
+void CSwordBoomerang::Start()
+{
+	CGameObject::Start();
+}
+
+bool CSwordBoomerang::Init()
+{
+	if (!CGameObject::Init())
+		return false;
+
+	m_MainSprite = CreateComponent<CSpriteComponent>("MainSprite");
+
+	m_MainSprite->SetRenderState("AlphaBlend");
+
+	CAnimationSequence2DInstance* AnimationInstance = m_Scene->GetResource()->LoadAnimationInstance(
+		"KirbyNormalSwordAttackEffect", TEXT("Ability_Sword_AttackEffect.anim"));
+
+	m_MainSprite->SetAnimationInstance(AnimationInstance);
+
+	m_MainSprite->SetWorldScale(120.f, 120.f, 1.f);
+
+	m_MainSprite->SetPivot(0.5f, 0.5f, 0.f);
+
+	m_Collider = CreateComponent<CColliderCircle>("Collider");
+	m_MainSprite->AddChild(m_Collider);
+
+	m_CurrentSpeed = m_InitSpeed;
+
+	return true;
+}
+
+void CSwordBoomerang::Update(float DeltaTime)
+{
+	CGameObject::Update(DeltaTime);
+
+	float MoveDist = (Vector3(m_AttackDir, 0.f, 0.f) * DeltaTime * m_CurrentSpeed).Length();
+
+	if (!m_Return)
+	{
+		m_CurrentSpeed -= m_SpeedAccel;
+
+		AddWorldPos(Vector3(m_AttackDir, 0.f, 0.f) * DeltaTime * m_CurrentSpeed);
+
+		m_ThrowDirDist += MoveDist;
+
+		if (m_ThrowDirDist >= m_ThrowDirDistMax)
+		{
+			m_ThrowDirDist = 0.f;
+
+			m_Return = true;
+		}
+	}
+	else
+	{
+		m_CurrentSpeed += m_SpeedAccel;
+
+		AddWorldPos(Vector3(m_AttackDir * -1.f, 0.f, 0.f) * DeltaTime * m_CurrentSpeed);
+
+		m_ThrowDirDist += MoveDist;
+
+		if (m_ThrowDirDist >= m_ThrowDirDistMax)
+		{
+			Destroy();
+		}
+	}
+}
+
+
+void CSwordBoomerang::CollisionCallback(const CollisionResult& Result)
+{
+	// 현재 충돌한 물체가 Block 이라면
+	if (Result.Dest->GetGameObject()->CheckType<CBlock>())
+	{
+		// Attack Back Effect
+		CFireAttackBackEffect* BackEffect = m_Scene->CreateGameObject<CFireAttackBackEffect>("BackFire");
+
+		BackEffect->SetWorldPos(GetWorldPos());
+
+		BackEffect->SetWorldScale(60.f, 60.f, 1.f);
+
+		BackEffect->GetColliderBody()->SetInfo(Vector2(0.f, 0.f), BackEffect->GetWorldScale().x * 0.5f);
+
+		BackEffect->AddRelativeRotationZ(90.f);
+
+		BackEffect->SetLifeTime(0.4f);
+	}
+
+	CColliderComponent* CollisionDest = Result.Dest;
+
+	CGameObject* Owner = CollisionDest->GetGameObject();
+
+	CWidgetComponent* ObjectWindow = nullptr;
+
+	if (Owner)
+	{
+		CMonster* DestMonster = dynamic_cast<CMonster*>(Owner);
+
+		if (!DestMonster)
+			return;
+
+		// HP Bar 달게 하기
+		DestMonster->Damage(m_AttackDamage);
+
+		DestMonster->SetBeingHit(true);
+
+		DestMonster->SetAIState(Monster_AI::Hit);
+
+		if (m_AttackDir.x < 0)
+			DestMonster->SetObjectMoveDir(Vector3(-1.f, 0.f, 0.f));
+		else
+			DestMonster->SetObjectMoveDir(Vector3(1.f, 0.f, 0.f));
+
+		// DestMonster->Damage(2.f);
+
+		// Create Damage Font
+		ObjectWindow = Owner->FindComponentByType<CWidgetComponent>();
+
+		if (ObjectWindow)
+		{
+			CUIDamageFont* DamageFont = ObjectWindow->GetWidgetWindow()->CreateUIWidget<CUIDamageFont>("DamageFont");
+
+			DamageFont->SetDamage((int)m_AttackDamage);
+		}
+	}
+}
---
+++ Client2D/Include/Object/SwordBoomerang.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "GameObject\GameObject.h"
+
+class CSwordBoomerang :
+    public CGameObject
+{
+	friend class CSwordKirbyState;
+protected:
+	CSwordBoomerang();
+	virtual ~CSwordBoomerang();
+protected:
+	float m_ThrowDirDist;
+	float m_ThrowDirDistMax;
+	bool m_Return;
+	float m_SpeedAccel;
+	float m_InitSpeed;
+	float m_CurrentSpeed;
+	float m_AttackDamage;
+	CSharedPtr<class CSpriteComponent> m_MainSprite;
+	CSharedPtr<class CColliderCircle> m_Collider;
+	float m_AttackDir;
+protected:
+	void SetAttackDamage(float Damage);
+	void SetAttackDir(float DirX);
+protected:
+	void UpdateBoomerangMovement(float DeltaTime);
+	void CollisionCallback(const CollisionResult& Result);
+protected:
+	virtual void Start() override;
+	virtual bool Init() override;
+	virtual void Update(float DeltaTime) override;
+};
+
