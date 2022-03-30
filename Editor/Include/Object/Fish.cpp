5ed483badf77b174f3b56ada30e4c763d2d06eb5
ohbumjun <beomjun0638@hanmail.net>
2022-03-30 14:59:31 +0900

    [Feat] (Editor) Create CFish Class Which Is An Another Water Monster

---
+++ Editor/Include/Object/Fish.cpp
@@ -0,0 +1,57 @@
+#include "Fish.h"
+#include "Component/WidgetComponent.h"
+#include "UI/MonsterEditorHUD.h"
+
+CFish::CFish()
+{
+	SetTypeID<CFish>();
+}
+
+CFish::~CFish()
+{}
+
+void CFish::Start()
+{
+	CWaterMonster::Start();
+
+	CWidgetComponent* Component = FindComponentByType<CWidgetComponent>();
+
+	if (Component)
+	{
+		CMonsterEditorHUD* MonsterHUD = dynamic_cast<CMonsterEditorHUD*>(Component->GetWidgetWindow());
+
+		if (MonsterHUD)
+		{
+			MonsterHUD->SetText(TEXT("Fish")); //
+		}
+	}
+}
+
+bool CFish::Init()
+{
+	if (!CWaterMonster::Init())
+		return false;
+
+	LoadAnimationInstance("Fish", TEXT("Fish.anim"));
+	SetCurrentAnimation("RightIdle");
+
+	CMonsterEditorHUD* MonsterHUD = dynamic_cast<CMonsterEditorHUD*>(m_SimpleHUDWidget->GetWidgetWindow());
+
+	if (MonsterHUD)
+	{
+		MonsterHUD->SetText(TEXT("Squid"));
+	}
+
+	return true;
+}
+
+void CFish::Update(float DeltaTime)
+{
+	CWaterMonster::Update(DeltaTime);
+}
+
+void CFish::PostUpdate(float DeltaTime)
+{
+	CWaterMonster::PostUpdate(DeltaTime);
+}
+
---
+++ Editor/Include/Object/Fish.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "WaterMonster.h"
+class CFish :
+    public CWaterMonster
+{
+    friend class CScene;
+protected:
+    CFish();
+    virtual ~CFish();
+public:
+    virtual void Start() override;
+    virtual bool Init() override;
+    virtual void Update(float DeltaTime);
+    virtual void PostUpdate(float DeltaTime);
+};
+
